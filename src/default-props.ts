5a5d58ed544d1a0616926dd37023f2fab84105fc
termer <termer@termer.net>
2026-07-13 02:08:50 -0400

    fix default for stroke not being set

--- src/default-props.ts
+++ src/default-props.ts
@@ -2,7 +2,7 @@ import { IconProps } from './icon-props'
 
 export const fallbackProps: IconProps = {
 	size: 24,
-	color: 'currentColor',
+	stroke: 'currentColor',
 	'stroke-width': 2,
 	'stroke-linecap': 'round',
 	'stroke-linejoin': 'round',
